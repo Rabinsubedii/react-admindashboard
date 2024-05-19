ab4a74e16d160c17b78d10d54be46819e9efeb60
Rabin Subedi <90462513+Rabinsubedii@users.noreply.github.com>
2024-05-19 12:51:35 +0545

    admin dashboard

--- src/App.js
+++ src/App.js
@@ -4,13 +4,36 @@ import 'remixicon/fonts/remixicon.css';
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import List from './pages/list/List';
+import New from './pages/new/New';
+import Single from './pages/single/Single';
 
 function App() {
-  return (
-    <Header/>
+  return (  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home/>} /> 
+          {/* User route */}
+            <Route path="users"> 
+              <Route index element={<List/>} />
+              <Route path=":userId" element={<Single/>} />
+              <Route path="new" element={<New/>} />
+            </Route> 
+
+            {/*Product Route  */}
+            <Route path="products"> 
+              <Route index element={<List/>} />
+              <Route path=":productId" element={<Single/>} />
+              <Route path="new" element={<New/>} />
+            </Route> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
