f544915b2235d97df55516b78ff278c3a5294959
Rabin Subedi <90462513+Rabinsubedii@users.noreply.github.com>
2024-05-19 23:45:44 +0545

    admin-dashboard

--- src/App.js
+++ src/App.js
@@ -2,36 +2,21 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'remixicon/fonts/remixicon.css';
 // import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/js/jquery.slim.min.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import List from './pages/list/List';
-import New from './pages/new/New';
-import Single from './pages/single/Single';
+import Header from './components/header/Header.js';
 
 function App() {
   return (  
     <BrowserRouter>
+    <Header/>
       <Routes>
-        <Route path="/">
           <Route index element={<Home/>} /> 
-          {/* User route */}
-            <Route path="users"> 
-              <Route index element={<List/>} />
-              <Route path=":userId" element={<Single/>} />
-              <Route path="new" element={<New/>} />
-            </Route> 
-
-            {/*Product Route  */}
-            <Route path="products"> 
-              <Route index element={<List/>} />
-              <Route path=":productId" element={<Single/>} />
-              <Route path="new" element={<New/>} />
-            </Route> 
-        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
+import React from 'react';
+import "./home.css"
 
 const Home = () => {
   return (
     <div className='home'>
-      <Sidebar/>
+      <div className="homeContainer">
+      </div>
     </div>
   )
 }
